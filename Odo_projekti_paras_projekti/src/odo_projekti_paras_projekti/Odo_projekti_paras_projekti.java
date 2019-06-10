4a786513b14b43cee9eeb517477325dc342d4550
Odocoder1 <51635882+Odocoder1@users.noreply.github.com>
2019-06-10 14:48:40 +0300

    Add files via upload

---
+++ Odo_projekti_paras_projekti/src/odo_projekti_paras_projekti/Odo_projekti_paras_projekti.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package odo_projekti_paras_projekti;
+
+/**
+ *
+ * @author guest-mdg8q9
+ */
+public class Odo_projekti_paras_projekti {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+System.out.println("Hello world");
+ }
+   
+    
+    
